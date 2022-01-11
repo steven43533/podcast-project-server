9b2cd1730ef1f857dc2bbc68b0edeee91789a901
steven43533 <steven43533@gmail.com>
2022-01-10 19:42:45 -0500

    Added new GET route for searching by genre

--- app/routes/ListenNotes.js
+++ app/routes/ListenNotes.js
@@ -4,16 +4,34 @@ const express = require('express')
 const axios = require('axios')
 
 const router = express.Router()
-var config = {
+let configByPodcast = {
 	method: 'GET',
-	url: 'https://listen-api.listennotes.com/api/v2/search?q=low%society&type=podcast',
+	url: 'https://listen-api.listennotes.com/api/v2/search?q=revolutions&type=podcast',
+	headers: {
+		"X-ListenAPI-Key": `${process.env.API_KEY}`
+	}
+}
+let configByGenre = {
+	method: 'GET',
+	url: 'https://listen-api.listennotes.com/api/v2/genres?top_level_only=1',
 	headers: {
 		"X-ListenAPI-Key": `${process.env.API_KEY}`
 	}
 }
 
-router.get('/', (req, res, next) => {
-	axios(config)
+
+
+router.get('/by-podcast', (req, res, next) => {
+	axios(configByPodcast)
+		.then(function(response) {
+			console.log(res.json(response.data))
+		})
+		.catch(function (error){
+			console.log(error);
+		})
+})
+router.get('/by-genre', (req, res, next) => {
+	axios(configByGenre)
 		.then(function(response) {
 			console.log(res.json(response.data))
 		})
